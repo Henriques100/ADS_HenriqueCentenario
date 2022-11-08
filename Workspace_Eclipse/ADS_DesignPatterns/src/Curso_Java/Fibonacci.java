8a3526c26622fc2f070fbcdff95008fb8820a218
Henriques100 <110470168+Henriques100@users.noreply.github.com>
2022-11-08 17:04:13 -0300

    Add files via upload

---
+++ Workspace_Eclipse/ADS_DesignPatterns/src/Curso_Java/Fibonacci.java
@@ -0,0 +1,18 @@
+package Curso_Java;
+
+public class Fibonacci {
+	static long fibo(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= 12; i++)
+		{
+            System.out.print("(" + i + "):" + Fibonacci.fibo(i) + "\t");
+		}
+	}
+}
---
+++ Workspace_Eclipse/ADS_DesignPatterns/src/Curso_Java/NumeroParOuImpar.java
@@ -0,0 +1,34 @@
+package Curso_Java;
+
+public class NumeroParOuImpar {
+
+    static int ParOuImpar(int x) {
+	if(x/2 == 0)
+	{
+		x = x/2;
+		return x;
+	}
+	else
+	{
+		x= 3*x +1;
+		return x;
+	}
+    }
+	public static void main(String[] args) {
+		int x = 13;
+		while(x != 1)
+		{
+			if(x/2 == 0)
+			{
+			 x = NumeroParOuImpar.ParOuImpar(x);
+			 System.out.println(x);
+			}
+			else
+			{
+			 x = NumeroParOuImpar.ParOuImpar(x);	
+			 System.out.println(x);
+			}
+			
+		}
+		}
+	}
