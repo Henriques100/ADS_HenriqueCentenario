c976bf7965902dfa01274d1164b8bf8f3d6373ea
Henriques100 <110470168+Henriques100@users.noreply.github.com>
2022-11-08 17:02:35 -0300

    Add files via upload

---
+++ Workspace_Eclipse/ADS_DesignPatterns/src/strategy_comparator/ComparadorPorId.java
@@ -0,0 +1,12 @@
+package strategy_comparator;
+
+import java.util.Comparator;
+
+public class ComparadorPorId implements Comparator<Animal> {
+
+	@Override
+	public int compare(Animal a1, Animal a2) {
+		// TODO Auto-generated method stub
+		return a1.getId().compareTo(a2.getId());
+	}
+}
